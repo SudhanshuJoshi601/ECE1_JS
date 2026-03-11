fb80970cfb786285e5aa890ae5584ba14532f7cb
SudhanshuJoshi601 <mabahuguna@gmail.com>
2026-03-11 09:44:05 +0530

    push1

---
+++ hello.js
@@ -0,0 +1,43 @@
+<script type="text/javascript">
+
+
+
+
+
+</script>
+
+for(key in object){
+
+}
+const person={
+
+    fname:"Ajay",
+    lname:"Singh",
+    age:25
+};
+for(let x in person){
+    console.log("person details:"+x+";"+person[x])
+}
+
+
+var age=20;
+
+if(age>18){
+    document.write("<b>You are eligible to cast vote</b>")
+};
+
+
+var grade="A";
+switch(grade){
+    case "A":document.write("good job<br/>");
+    break;
+    case "B":document.write("pretty job<br/>");
+    break;
+    case "C":document.write("passed<br/>");
+    break;
+    case "D":document.write("Not so good<br/>");
+    break;
+    default:document.write("good job<br/>");
+    break;
+
+}
